17952bc90e4aad29500f2af73b428ff9a2fc47bc
hayksakulyan <sakulyanhayk@gmail.com>
2023-06-10 15:40:55 +0400

    "First commit, added clock files"

---
+++ Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Hayk Sakulyan on 10.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
